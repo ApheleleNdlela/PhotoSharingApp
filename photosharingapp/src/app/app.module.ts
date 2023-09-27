ec051a3d202560ec5bdbf2c2f657c630ebbe0eac
VuyoSopa <vssopangisa@gmail.com>
2023-09-27 15:34:24 +0200

    updates on cards

--- photosharingapp/src/app/app.module.ts
+++ photosharingapp/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewComponent } from './view/view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ViewComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
