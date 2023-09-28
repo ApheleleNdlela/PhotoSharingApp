a36c1b459c062e61f53e94ead3aa8336256c92df
Busiswa <songcabusiswa34@gmail.com>
2023-09-28 17:22:32 +0200

    update logn html

--- photosharingapp/src/app/app-routing.module.ts
+++ photosharingapp/src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ViewComponent } from './view/view.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddProfileComponent } from './add-post/add-profile.component';
-<<<<<<< HEAD
-=======
 import { RegisterComponent } from './register/register.component';
->>>>>>> 2f40bbd4a2bd9d0bc765290afc6ce6844b0f4904
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
