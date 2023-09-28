6d2a47adcc25233aa22f7d3a621dc2b0ab3c4c73
VuyoSopa <vssopangisa@gmail.com>
2023-09-28 14:22:44 +0200

    view component

--- photosharingapp/src/app/view/view.component.ts
+++ photosharingapp/src/app/view/view.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BackendApiService } from '../services/backend-api.service';
 
 
 
@@ -7,6 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css'],
 })
-export class ViewComponent {
+export class ViewComponent implements OnInit {
+  constructor(private BackendApiService: BackendApiService, private router: Router ) {}
 
+  availableImages?: any;
+
+  ngOnInit(): void {
+    this.getallPosts();
+  }
+
+  getallPosts() {
+    this.BackendApiService.getallPosts().subscribe({
+      next: (res) => {
+        this.availableImages = res;
+      },
+    });
+  }
+
+  deletePost(id: number): void {
+    alert ('Are you sure you want to delete?');
+    this.BackendApiService.deletePost(id).subscribe({
+      next: () => {
+        this.getallPosts()
+      },
+      error: console.log,
+    });
+  }
 }
