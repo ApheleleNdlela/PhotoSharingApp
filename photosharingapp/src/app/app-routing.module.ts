4c034d5c4c24dff90d4a9aee3d1b98ec6bc3fe3c
Aphelele_Ndlela <aphelelendlela95@gmail.com>
2023-09-28 10:51:19 +0200

    AddProfileComponent

--- photosharingapp/src/app/app-routing.module.ts
+++ photosharingapp/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewComponent } from './view/view.component';
+import { AddProfileComponent } from './add-profile/add-profile.component';
 
 const routes: Routes = [
 
   {path: "", redirectTo: "app", pathMatch:"full"},
 
   {path:"view", component: ViewComponent},
+  {path: "profile", component: AddProfileComponent}
 ];
 
 @NgModule({
--- photosharingapp/src/app/app.module.ts
+++ photosharingapp/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewComponent } from './view/view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddProfileComponent } from './add-profile/add-profile.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    AddProfileComponent,
+
     
   ],
   imports: [
@@ -23,7 +32,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDialogModule
 
   ],
   providers: [],
