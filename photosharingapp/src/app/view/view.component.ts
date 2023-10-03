a8b8370c45fa82a23645ff8e4246ec116881c790
Aphelele_Ndlela <aphelelendlela95@gmail.com>
2023-10-03 08:48:50 +0200

    Grid view

--- photosharingapp/src/app/view/view.component.ts
+++ photosharingapp/src/app/view/view.component.ts
@@ -1,8 +1,8 @@
-import { Component,OnInit, OnInit } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import { BackendApiService } from '../services/backend-api.service';
 import { AddProfileComponent } from '../add-post/add-profile.component';
-import { BackendApiService } from '../services/backend-api.service';
+
 
 
 
@@ -12,33 +12,19 @@ import { BackendApiService } from '../services/backend-api.service';
   styleUrls: ['./view.component.css'],
 })
 export class ViewComponent implements OnInit {
-  constructor(private BackendApiService: BackendApiService, private router: Router ) {}
-
-  availableImages?: any;
-
-  ngOnInit(): void {
-    this.getallPosts();
-  }
-
-  getallPosts() {
-    this.BackendApiService.getallPosts().subscribe({
-      next: (res) => {
-        this.availableImages = res;
-      },
-    });
-  }
+  
 
   deletePost(id: number): void {
     alert ('Are you sure you want to delete?');
-    this.BackendApiService.deletePost(id).subscribe({
+    this.service.deletePost(id).subscribe({
       next: () => {
         this.getallPosts()
       },
       error: console.log,
     });
   }
-  User:any = {
-export class ViewComponent implements OnInit{
+
+
 
 
   posts: any;
