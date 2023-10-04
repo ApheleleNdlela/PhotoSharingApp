848dd59f2a50bb03be9fba027e25da11927c5451
VuyoSopa <vssopangisa@gmail.com>
2023-10-04 10:52:36 +0200

    login

--- photosharingapp/src/app/login/login.component.ts
+++ photosharingapp/src/app/login/login.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service'
+import { StorageService } from 'src/app/services/storage.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent  {
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+
+  constructor(
+    
+    private authService: AuthService,
+    private storageService: StorageService
+    ) {}
+
+  ngOnInit() {
+    // this.user.currentUserData.subscribe(userData => (this.userData = userData));
+    if (this.storageService.isLoggedIn()) {
+      this.isLoggedIn = true;
+      // this.roles = this.storageService.getUser().roles;
+    }
+  }
+
+  changeData(event:any) {
+    var msg = event.target.value;
+    // this.user.changeData(msg);
+  }
+
+  
+  login() {
+    // this.user.changeData(data);
+    const { username, password } = this.form
+
+    this.authService.login(username, password).subscribe({
+      next: data => {
+        console.log(data);
+        
+        this.storageService.saveUser(data);
+        this.storageService.saveToken(data.token)
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        window.location.replace("user-profile") // last line
+      },
+      error: err => {
+        console.error("Details required")
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    })
+    
+  }
 
 }
